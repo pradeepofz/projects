8179baa6d2dafee2a597fd0156010c289544f447
Pradeep <pradeepofz1@gmail.com>
2021-11-21 18:51:03 +0530

    project

---
+++ Calculator/script.js
@@ -0,0 +1,58 @@
+// const para = document.querySelector('.para');
+// para.innerHTML='hey'
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButton = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+class Calculator{
+    constructor(previousOperandTextElement,currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+    clear(){
+        this.previousOperand = '';
+        this.currentOperand = '';
+        this.operation = undefined;
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+        this.currentOperand = number;
+
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.currentOperand;
+
+    }
+}
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', ( )=> {
+        calculator.appendNumber(button.innerText)  //number
+        calculator.updateDisplay();
+    });
+});
+
+
+
