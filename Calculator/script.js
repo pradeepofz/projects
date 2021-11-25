e3a9d1e43054defb89b895450adf23c128d3e003
pradeepofz <pradeepofz1@gmail.com>
2021-11-25 09:19:06 +0530

    operation Button

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,44 +2,47 @@
 // para.innerHTML='hey'
 
 const numberButtons = document.querySelectorAll('[data-number]');
-const operationButton = document.querySelectorAll('[data-operation]');
+const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
-class Calculator{
-    constructor(previousOperandTextElement,currentOperandTextElement){
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
         this.clear();
     }
 
-    clear(){
+    clear() {
         this.previousOperand = '';
         this.currentOperand = '';
         this.operation = undefined;
     }
 
-    delete(){
+    delete() {
 
     }
 
-    appendNumber(number){
-        this.currentOperand = number;
+    appendNumber(number) {
+        // this.currentOperand = number;
+        // if (number === '.' && this.currentOperand.includes('.')) 
+        // return this.currentOperand = this.currentOperand.toString() + number.toString()
+        if( number === '.'  && this.currentOperand.includes('.')) return
+         this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation(operation) {
 
     }
 
-    chooseOperation(operation){
+    compute() {
 
     }
 
-    compute(){
-
-    }
-
-    updateDisplay(){
+    updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand;
 
     }
@@ -48,8 +51,15 @@ class Calculator{
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
 numberButtons.forEach(button => {
-    button.addEventListener('click', ( )=> {
+    button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)  //number
+        calculator.updateDisplay();
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)  
         calculator.updateDisplay();
     });
 });
